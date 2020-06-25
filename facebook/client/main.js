6f95ed31b635e593464b61621fb4e291e558acc2
kfethan <jarrette2@winthrop.edu>
2020-06-25 02:00:14 -0400

    facebook added

--- facebook/client/main.js
+++ facebook/client/main.js
@@ -1,6 +1,5 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-import {UP_Collection_Access} from './../imports/api/user_post.js';
 
 import './main.html';
 
@@ -19,6 +18,5 @@ Template.hello.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
-      console.log(UP_Collection_Access.find().fetch());
   },
 });
--- reddit/server/main.js
+++ reddit/server/main.js
@@ -1,5 +1,15 @@
 import { Meteor } from 'meteor/meteor';
 
+import {UP_Collection_Access} from './../imports/api/user_post.js';
+
 Meteor.startup(() => {
-  // code to run on server at startup
+  UP_Collection_Access.insert({
+    topic: 'Jake G',
+    movie: 7,
+  });
+  UP_Collection_Access.insert({
+    topic: 'Paul Walker',
+    movie: 'fast and furious'
+  })
+  console.log(UP_Collection_Access.find().fetch());
 });
